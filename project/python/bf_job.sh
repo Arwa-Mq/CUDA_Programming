df0eb7f8a61eec56bde99dbd64e51993bc425a10
Arwa Qadi <76020791+Arwa-Mq@users.noreply.github.com>
2025-07-24 20:12:13 +0100

    python on gpu

--- project/python/bf_job.sh
+++ project/python/bf_job.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name=cosmosis_gpu_boost
+#SBATCH --job-name=cosmosis_gpu_boostf
 #SBATCH --output=cosmosis_gpu_boost_%j.out
 #SBATCH --error=cosmosis_gpu_boost_%j.err
 #SBATCH --qos=regular                # or 'debug' for short runs
 #SBATCH --nodes=1
 #SBATCH --gpus=1                     # Request 1 GPU
 #SBATCH --cpus-per-task=4           # Match your emcee config (10 walkers, 4 CPUs)
-#SBATCH --time=02:00:00             # Max runtime (adjust as needed)
+#SBATCH --time=01:00:00             # Max runtime (adjust as needed)
 #SBATCH --constraint=gpu
-#SBATCH --account=your_project_name_here
+
 
 # Load environment
-module load cuda
-module load python             # Or your preferred module set
+#module restore cosmosis
+
 
 # Activate your virtual environment if needed
 # source /path/to/venv/bin/activate
 
 # Move to working directory
-cd /global/homes/a/arwa_mq/DESy3/Boost_factor_cosmosis
+cd /its/home/aa3044/CosmoSIS_2025
+source cosmosis_env/bin/activate
+source cosmosis-configure
 
 # (Optional) Confirm GPU is visible
 nvidia-smi
 
 # Run the pipeline
-cosmosis boost_factor.ini
+cosmosis /its/home/aa3044/CUDA_course/CUDA_Programming/project/boost_factor.ini
---
+++ project/python/bf_like_cuda.py
@@ -0,0 +1,61 @@
+# this like can be run on GPU using CUDA.. it is using a mock data 
+import cupy as cp
+from cosmosis.datablock import names
+
+def Boost_Factor_Model(R, rs, b0):
+    x = R / rs
+    fx = cp.zeros_like(x)
+
+    # Avoid dividing by 0 by masking regions
+    gt1 = x > 1
+    eq1 = cp.isclose(x, 1)
+    lt1 = x < 1
+
+    fx[gt1] = cp.arctan(cp.sqrt(x[gt1]**2 - 1)) / cp.sqrt(x[gt1]**2 - 1)
+    fx[eq1] = 1.0
+    fx[lt1] = cp.arctanh(cp.sqrt(1 - x[lt1]**2)) / cp.sqrt(1 - x[lt1]**2)
+
+    denominator = x**2 - 1
+    denominator = cp.where(cp.isclose(denominator, 0.0), 1e-10, denominator)
+
+    B = 1 + b0 * (1 - fx) / denominator
+    B = cp.where(cp.isnan(B), (b0 + 3) / 3.0, B)
+    return B
+
+def setup(options):
+    R = cp.logspace(-1, 2, 100)
+    B0 = 0.3
+    Rs = 0.5
+    data_B = Boost_Factor_Model(R, Rs, B0)
+
+    variance = cp.ones(data_B.size) * 0.1**2
+    sigma_B = data_B * cp.random.normal(loc=0, scale=cp.sqrt(variance))
+
+    config = {
+        'R': R,
+        'data_vector': data_B,
+        'sigma_B': sigma_B,
+        'covariance': variance,
+    }
+
+    return config
+
+def execute(block, config):
+    R = config['R']
+    data_vector = config['data_vector']
+    sigma_B = config['sigma_B']
+
+    logrs = block["Boost_Factor_Model_Values", "logrs_00"]
+    logb0 = block["Boost_Factor_Model_Values", "logb0_00"]
+
+    rs = 10**logrs
+    b0 = 10**logb0
+
+    model_prediction = Boost_Factor_Model(R, rs, b0)
+    chisq = cp.sum(((model_prediction - data_vector) / sigma_B) ** 2)
+    log_L = -0.5 * chisq
+
+    # Move scalar back to CPU for CosmoSIS
+    block["likelihoods", "boost_factor_likelihood_like"] = log_L.get()
+
+    return 0
